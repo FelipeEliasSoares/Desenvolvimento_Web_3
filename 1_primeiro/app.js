55a8c43f3c6aeba3661e55a839c5e127f8054654
FelipeEliasSoares <soaresfelipe396@gmail.com>
2024-09-09 10:13:40 -0300

    :white_check_mark: feat: 1_primeiro

---
+++ 1_primeiro/app.js
@@ -0,0 +1,24 @@
+//@ Importa as bibliotecas
+const express = require("express");
+require("dotenv").config();
+
+//@ Configura o servidor
+const app = express();
+const port = process.env.PORT;
+
+//@ Cria uma rota para o endereço raiz.
+app.get("/", (req, res) => {
+  res.send("Hello DW3!");
+});
+
+//@ Cria uma rota para o endereço raiz.
+app.get("/dw3", (req, res) => {
+  res.send("Fera demais!!!");
+});
+
+//@ Inicia o servidor
+app.listen(port, () => {
+  console.log("Executando a aplicação:", process.env.APP_NAME);
+
+  console.log("Example app listening on port:", port);
+});
